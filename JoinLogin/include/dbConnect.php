b6b774cf6664887f779dc252da78be7abea10adb
jamsya <jamsya@naver.com>
2017-10-12 21:43:15 +0900

    첫페이지 작성 완료. 나머지 기능들을 구현한다.

---
+++ JoinLogin/include/dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$passWord = 'dbdb';
+$dbName = 'dbMember';
+
+$dbConnect = new mysqli($host,$user,$passWord,$dbName);
+
+
+?>
--- join/joinmember.php
+++ join/joinmember.php
@@ -7,10 +7,10 @@
 <h2><br><center>join page</center>
     <form method="post" action="join_result.php">
         <b>ID</b><input type="text" size=10 maxlength=10 name="id"><br />
-        <b>name</b><input type="text" style="ime-mode: active" size=10 maxlength=10 name="yourname"><br />
-        <b>password</b><input type="password" size=10 maxlength=10 name="pwd"><br />
-        <b>confirm password</b><input type="password" size=10 maxlength=10 name="pwd2"><br />
-        <b>cell phone number</b>
+        <b>이름</b><input type="text" style="ime-mode: active" size=10 maxlength=10 name="yourname"><br />
+        <b>비밀번호</b><input type="password" size=10 maxlength=10 name="pwd"><br />
+        <b>비밀번호 확인</b><input type="password" size=10 maxlength=10 name="pwd2"><br />
+        <b>휴대전화</b>
 
         <select name="m1">
             <option value=" ">select</option>
@@ -26,13 +26,13 @@
         -
         <input type="text" size=4 name="m3" maxlength=4><br />
 
-        <b>sex</b>
+        <b>성별</b>
         <input type="radio" name="sex" value="man" checked>man
         <input type="radio" name="sex" value="woman">woman <br />
 
-        <b>address</b><input type="text" size=60 name="addr"><br />
+        <b>주소</b><input type="text" size=60 name="addr"><br />
 
-        <b>your hobby</b>
+        <b>취미</b>
         <input type="checkbox" name="com" value="yes" checked> computer
         <input type="checkbox" name="sports" value="yes"> sports
         <input type="checkbox" name="shop" value="yes"> shopping
@@ -40,7 +40,7 @@
 
 
 
-        <input type="submit" value="abcd" value="join">
-        <input type="reset" value="rewrite">
+        <input type="submit" value="회원가입" value="join">
+        <input type="reset" value="다시 작성하기">
     </form>
 </html>
